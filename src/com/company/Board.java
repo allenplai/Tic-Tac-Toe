246337a40d9683f901caff75869f33d943aecce6
Allen Lai <allenplai@gmail.com>
2017-06-26 23:30:54 -0400

    AI improved

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -7,7 +7,7 @@ public class Board {
 
     private char[][] board;
 
-
+    // counts to keep track if there is a winner
     private int[] rowsCount;    // X is +1 , O is -1
     private int[] columnCount;    // X is +1, O is -1
     private int diagnalCount;
@@ -27,15 +27,16 @@ public class Board {
         }
     }
 
-
+    // return false for invalid input
     public boolean addToken(char type, int row, int column) {
         if (board[row][column] == '-'){
             if (row <= 2 && column <= 2) {
                 if (row >= 0 && column >= 0) {
                     board[row][column] = type;
-
                     keepScore(type, row, column);
-                    didPlayerWin(type,row,column);
+                    if (didAnyPlayerWin(type,row,column) != '-') {
+                        System.exit(0);
+                    }
                     return true;
                 }
             }
@@ -43,7 +44,7 @@ public class Board {
         return false;
     }
     public void keepScore(char type, int row, int column) {
-        // keep score
+
         int toAdd = 0;
         if (type == 'X') {
             toAdd = 1;
@@ -90,26 +91,82 @@ public class Board {
         if (isBoardFull()) {
             return false;
         }
+        // 1. if AI can win in the first move place it there,   no need to check if addToken will return true bc program terminates
+        for (int i = 0; i < 3; i++) {       // row col
+            if (rowsCount[i] == -2) {
+                // add the token to the right place.
+                for (int j = 0; j < 3; j++) {
+                    addToken('O', i, j);
+                }
+            }
 
+            if (columnCount[i] == -2) {
+                // add the token to the right place.
+                for (int j = 0; j < 3; j++) {
+                    addToken('O', i, j);
+                }
+            }
+        }
+        if (diagnalCount == -2) {
+            for (int i = 0; i < 3; i++) {
+                addToken('O', i, i);
+            }
+        }
+        if (antiDiagnalCount == -2) {
+            for (int i = 0; i < 3; i++) {
+                addToken('O', 3-i-1, i);
+            }
+        }
+
+
+        // 2. AI blocks the user
+        for (int i = 0; i < 3; i++) {
+            if (rowsCount[i] == 2) {
+                // add the token to the right place.
+                for (int j = 0; j < 3; j++) {
+                    if (addToken('O', i, j)) return true;
+                }
+            }
+
+            if (columnCount[i] == 2) {
+                // add the token to the right place.
+                for (int j = 0; j < 3; j++) {
+                    if (addToken('O', i, j)) return true;
+                }
+            }
+        }
+        if (diagnalCount == 2) {
+            for (int i = 0; i < 3; i++) {
+                if (addToken('O', i, i)) return true;
+            }
+        }
+        if (antiDiagnalCount == 2) {
+            for (int i = 0; i < 3; i++) {
+                if (addToken('O', 3-i-1, i)) return true;
+            }
+        }
+
+        // 3. if AI just make a move anywhere free
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i][j] == '-') {
-                    board[i][j] = 'O';
+                    addToken('O',i,j);
                     return true;
                 }
             }
         }
-        return true;
 
+        return true;
     }
 
-    public char didPlayerWin (char type, int row, int column) {
+    public char didAnyPlayerWin (char type, int row, int column) {
         if (Math.abs(rowsCount[row]) == 3 ||
                 Math.abs(columnCount[column]) == 3 ||
                 Math.abs(diagnalCount) == 3  ||
                 Math.abs(antiDiagnalCount) == 3)
         {
-            System.out.println(type + " player won!!!");
+            board.toString();
+            System.out.println("Player " + type + " wins!!");
             return type;
         }
         return '-';
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    // 3 x 3 Tic Tac Toe. User is always 'X' and starts first
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-
         Board newBoard = new Board();
+
 
         while (!newBoard.isBoardFull()) {
             System.out.println(newBoard.toString());
             boolean validIntput = false;
 
+            // 1. get user input to start playing Tic-Tac-Toe
             System.out.println("Enter in row number: (0-2)");
             int row=sc.nextInt();
             System.out.println("Enter in column number: (0-2)");
             int column=sc.nextInt();
             validIntput = newBoard.addToken('X',row,column);
 
+            // 2. if input is invalid, get user to try again
             while (!validIntput) {
                 System.out.println("Enter in row number: (0-2)");
                 int row1=sc.nextInt();
@@ -26,15 +31,13 @@ public class Main {
                 int column1=sc.nextInt();
                 validIntput = newBoard.addToken('X',row1,column1);
                 System.out.println("Invalid input, try again");
-
             }
+            // 3. Computers turn to make a move
             System.out.println(newBoard.toString());
             System.out.println("AI makes move:");
-
             newBoard.aiMakeMove();
-        }
-        // determine who won
 
+        }
 
 
     }
